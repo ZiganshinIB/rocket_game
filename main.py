402465dc1be7f139e8cd5fed82d0903281514fdb
ZiganshinIB <elzig3012@gmail.com>
2024-07-05 20:22:42 +0300

    Refract global name

--- main.py
+++ main.py
@@ -4,33 +4,36 @@ import time
 import asyncio
 import curses
 import curses_tools
+import obstacles
 import physics
 from explosion import explode
 from obstacles import Obstacle
 
-TIC_TIMEOUT = 0.1
 YEAR = 1957
-OBSTACLES = []
-OBSTACLES_COLLISIONS = []
-SPEEDS = [0, 0]
+tic_timeout = 0.1
+obstacles = []
+obstacles_collisions = []
+speeds = [0, 0]
 
-COROUTINES = []
-GARBAGE_FRAMES = []
-SPACESHIP_FRAMES = []
-GAME_OVER_FRAME = ''
+coroutines = []
+# garbage_frames = []
+garbage_frames = []
+# spaceship_frames = []
+spaceship_frames = []
+game_over_frame = ''
 
 
 async def sleep(tics=1):
-    """Sleep for 'tics' tics. 1 tic = TIC_TIMEOUT seconds"""
+    """Sleep for 'tics' tics. 1 tic = tic_timeout seconds"""
     for _ in range(tics):
         await asyncio.sleep(0)
 
 
 async def game_over(canvas):
-    rows, columns = curses_tools.get_frame_size(GAME_OVER_FRAME)
+    rows, columns = curses_tools.get_frame_size(game_over_frame)
     max_row, max_column = canvas.getmaxyx()
     while True:
-        curses_tools.draw_frame(canvas, (max_row - rows) / 2, (max_column - columns) / 2, GAME_OVER_FRAME)
+        curses_tools.draw_frame(canvas, (max_row - rows) / 2, (max_column - columns) / 2, game_over_frame)
         await sleep()
 
 
@@ -58,9 +61,9 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
 
     while 0 < row < max_row and 0 < column < max_column:
         canvas.addstr(round(row), round(column), symbol)
-        for obstacle in OBSTACLES:
+        for obstacle in obstacles:
             if obstacle.has_collision(row, column):
-                OBSTACLES_COLLISIONS.append(obstacle)
+                obstacles_collisions.append(obstacle)
                 canvas.addstr(round(row), round(column), ' ')
                 return
         await sleep()
@@ -70,31 +73,31 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
 
 
 async def animate_spaceship(canvas, row, column):
-    rows, columns = curses_tools.get_frame_size(SPACESHIP_FRAMES[1])
+    rows, columns = curses_tools.get_frame_size(spaceship_frames[1])
     max_row, max_column = canvas.getmaxyx()
     next_row = row
     next_column = column
     previous_frame_index = 0
-    x_speed, y_speed = SPEEDS
+    x_speed, y_speed = speeds
     while True:
         rows_direction, columns_direction, space_pressed = curses_tools.read_controls(canvas)
         if space_pressed and YEAR >= 2020:
-            COROUTINES.append(
+            coroutines.append(
                 fire(canvas, row, column + 2)
             )
         x_speed, y_speed = physics.update_speed(x_speed, y_speed, rows_direction, columns_direction)
         next_row = min(max(1, next_row + x_speed), max_row - rows - 1)
         next_column = min(max(1, next_column + y_speed), max_column - columns - 1)
-        for index in range(len(SPACESHIP_FRAMES)):
-            curses_tools.draw_frame(canvas, row, column, SPACESHIP_FRAMES[previous_frame_index], negative=True)
-            for obstacle in OBSTACLES:
+        for index in range(len(spaceship_frames)):
+            curses_tools.draw_frame(canvas, row, column, spaceship_frames[previous_frame_index], negative=True)
+            for obstacle in obstacles:
                 if obstacle.has_collision(
                         int(row), int(column), obj_size_rows=rows, obj_size_columns=columns):
-                    COROUTINES.append(explode(canvas, row + int(rows / 2), column + int(columns / 2)))
+                    coroutines.append(explode(canvas, row + int(rows / 2), column + int(columns / 2)))
                     await game_over(canvas)
                     return
             next_frame_index = (previous_frame_index + index) % 2
-            curses_tools.draw_frame(canvas, next_row, next_column, SPACESHIP_FRAMES[next_frame_index], negative=False)
+            curses_tools.draw_frame(canvas, next_row, next_column, spaceship_frames[next_frame_index], negative=False)
             previous_frame_index = next_frame_index
             row = next_row
             column = next_column
@@ -107,22 +110,12 @@ async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
     frame_rows, frame_columns = curses_tools.get_frame_size(garbage_frame)
     column = min(max(column, 0), columns_number - frame_columns - 1)
     row = 1
-    obstacle = Obstacle(row, column, 1, frame_columns)
-    OBSTACLES.append(obstacle)
-    while row <= frame_rows:
-        if obstacle in OBSTACLES_COLLISIONS:
-            break
-        tmp_frame = "\n".join(garbage_frame.split("\n")[-int(row):])
-        curses_tools.draw_frame(canvas, 1, column, tmp_frame)
-        obstacle.rows_size = int(row)
-        await sleep()
-        curses_tools.draw_frame(canvas, 1, column, tmp_frame, negative=True)
-        row += speed
-    row = 1
+    obstacle = Obstacle(row, column, frame_rows, frame_columns)
+    obstacles.append(obstacle)
     while row < rows_number:
-        if obstacle in OBSTACLES_COLLISIONS:
-            COROUTINES.append(explode(canvas, row + int(frame_rows/2), column + int(frame_columns/2)))
-            OBSTACLES_COLLISIONS.remove(obstacle)
+        if obstacle in obstacles_collisions:
+            coroutines.append(explode(canvas, row + int(frame_rows/2), column + int(frame_columns/2)))
+            obstacles_collisions.remove(obstacle)
             break
         curses_tools.draw_frame(canvas, row, column, garbage_frame)
         obstacle.row = int(row)
@@ -133,7 +126,7 @@ async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
             garbage_frame = "\n".join(garbage_frame.split("\n")[:-1])
         row += speed
 
-    OBSTACLES.remove(obstacle)
+    obstacles.remove(obstacle)
 
 
 async def show_year(canvas):
@@ -161,7 +154,7 @@ async def show_year(canvas):
 async def blink(canvas, row, column, symbol='*', delay=0.1):
     """Draw the specified symbol in a blink"""
     while True:
-        await sleep(round(delay/TIC_TIMEOUT))
+        await sleep(round(delay / tic_timeout))
         canvas.addstr(row, column, symbol, curses.A_DIM)
         await sleep(20)
         canvas.addstr(row, column, symbol)
@@ -178,9 +171,9 @@ async def fill_orbit_with_garbage(canvas):
         if YEAR < 1961:
             await sleep()
         else:
-            frame = random.choice(GARBAGE_FRAMES)
+            frame = random.choice(garbage_frames)
             rows, columns = curses_tools.get_frame_size(frame)
-            COROUTINES.append(
+            coroutines.append(
                 fly_garbage(canvas, random.randint(1, columns_number - columns - 2), frame)
             )
             await sleep(get_garbage_delay_tics(YEAR))
@@ -206,10 +199,10 @@ def get_garbage_delay_tics(year):
 def draw(canvas):
     max_row, max_col = canvas.getmaxyx()
     canvas.border()
-    canvas.timeout(int(TIC_TIMEOUT * 1000))
+    canvas.timeout(int(tic_timeout * 1000))
     canvas.nodelay(True)
     curses.curs_set(False)
-    COROUTINES.extend([
+    coroutines.extend([
         animate_spaceship(canvas,
                           row=max_row // 2,
                           column=max_col // 2,),
@@ -217,35 +210,35 @@ def draw(canvas):
         show_year(canvas)
     ])
     # star animation
-    COROUTINES.extend([blink(canvas=canvas,
+    coroutines.extend([blink(canvas=canvas,
                              row=random.randint(0, max_row - 1),
                              column=random.randint(0, max_col - 1),
                              delay=random.random()*3) for _ in range(100)])
-    # COROUTINES.append(show_obstacles(canvas, OBSTACLES))
+    coroutines.append(obstacles.show_obstacles(canvas, obstacles))
     while True:
-        for coroutine in COROUTINES.copy():
+        for coroutine in coroutines.copy():
             try:
                 coroutine.send(None)
             except StopIteration:
-                COROUTINES.remove(coroutine)
+                coroutines.remove(coroutine)
 
         canvas.refresh()
-        time.sleep(TIC_TIMEOUT)
+        time.sleep(tic_timeout)
 
 
 if __name__ == '__main__':
     for file_name in os.listdir("sprites/garbage"):
         if file_name.endswith(".txt"):
             with open(os.path.join("sprites/garbage", file_name)) as file:
-                GARBAGE_FRAMES.append(file.read())
+                garbage_frames.append(file.read())
     with open(os.path.join('sprites', 'rocket_frame_1.txt')) as file:
-        SPACESHIP_FRAMES.append(file.read())
+        spaceship_frames.append(file.read())
 
     with open(os.path.join('sprites', 'rocket_frame_2.txt')) as file:
-        SPACESHIP_FRAMES.append(file.read())
+        spaceship_frames.append(file.read())
 
     with open(os.path.join('sprites', 'gameover.txt')) as file:
-        GAME_OVER_FRAME = file.read()
+        game_over_frame = file.read()
 
     curses.update_lines_cols()
     curses.wrapper(draw)
